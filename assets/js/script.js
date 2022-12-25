40df6523571a879796ac94d0490f1311f78af27f
ttrgear <t_tharpe@aol.com>
2022-12-25 00:33:45 -0600

    html,css, and js work3

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,4 +6,48 @@ let msg = document.getElementById("msg");
 let tasks = document.getElementById("tasks");
 let add = document.getElementById("add");
 
+form.addEventListener("submit" , (e) => {
+    e.preventDefault();
+    formValidation();
+});
 
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    formValidation();
+  });
+  
+  let formValidation = () => {
+    if (textInput.value === "") {
+      console.log("failure");
+      msg.innerHTML = "Task cannot be blank";
+    } else {
+      console.log("success");
+      msg.innerHTML = "";
+      acceptData();
+    }
+  };
+
+  let data = {};
+
+  let acceptData = () => {
+    data["text"] = textInput.value;
+    data["date"] = dateInput.value;
+    data["description"] = textarea.value;
+
+    createTasks();
+  };
+
+let createTasks = () => {
+    tasks.innerHTML += ` 
+    <div>
+        <span class="fw-bold">${data.text}</span>
+        <span class="small text-secondary">${data.date}</span>
+        <p>${data.description}</p>
+
+        <span class="options">
+            <i class="fas fa-edit"></i>
+            <i class="fas fa-trash-alt"></i>
+         </span>
+    </div>
+      `;
+};
