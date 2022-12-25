34c069b2b3281ae4db43f6043fbab1709c037dd0
ttrgear <t_tharpe@aol.com>
2022-12-24 22:39:12 -0600

    html, css , and js work2

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+let form = document.getElementById("form");
+let textInput = document.getElementById("textInput");
+let dateInput = document.getElementById("dateInput");
+let textarea = document.getElementById("textarea");
+let msg = document.getElementById("msg");
+let tasks = document.getElementById("tasks");
+let add = document.getElementById("add");
+
+
